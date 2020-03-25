15a0afc7ef41bd02f342d41cbd42c200dfc3f216
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-25 15:18:48 -0400

    brought in css, brought in static user data from github and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
 
-function App() {
-  return <div className='App'></div>;
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Navbar title='User Cards' icon='fab fa-github' />
+        <div class='container'>
+          <Users />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
