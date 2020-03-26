0e7c595d056491166aa5597cf0890c38cc7482eb
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-26 00:26:03 -0400

    refactoring to get state mapped through

--- src/App.js
+++ src/App.js
@@ -5,29 +5,36 @@ import './App.css';
 
 class App extends Component {
   state = {
-    users: 'testing'
+    users: [],
+    loading: true
   };
 
   async componentDidMount() {
     try {
+      this.setState({ loading: true });
+
       const res = await fetch('https://randomuser.me/api/?results=1');
       const data = await res.json();
+      console.log('This is data: ', data);
+      console.log('This is datavalue', data.results[0]);
       this.setState({
-        users: data.value
+        users: data,
+        loading: false
       });
     } catch (error) {
       this.setState({
-        users: error.message
+        data: error.message
       });
     }
   }
 
   render() {
+    console.log('this is state: ', this.state);
     return (
       <div className='App'>
         <Navbar title='User Cards' icon='fab fa-github' />
         <div class='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
