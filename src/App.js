2988f24a0ee5f8f39ba51d5da387d646451e4969
Patrick Furtak <patrick@patrickfurtak.com>
2020-03-27 12:04:15 -0400

    added in bloomer user cards, need to add in navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
-import './App.css';
+import 'bulma/css/bulma.css';
 
 class App extends Component {
   state = {
--- src/components/users/UserItem.jsx
+++ src/components/users/UserItem.jsx
@@ -1,24 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {
+  Card,
+  CardHeader,
+  CardImage,
+  CardContent,
+  Media,
+  MediaLeft,
+  MediaContent,
+  Title,
+  Subtitle,
+  Content,
+  Image
+} from 'bloomer';
 
 const UserItem = ({ user: { phone, email, location, name, picture } }) => {
   return (
-    <div className='card text-center'>
-      <img
-        src={picture.thumbnail}
-        alt=''
-        className='round-img'
-        style={{ width: '60px' }}
-      />
-      <h2>
-        {name.title} {name.first} {name.last}
-      </h2>
-      <h4>Email: {email}</h4>
-      <h4>
-        Location: {location.city}, {location.country}
-      </h4>
-      <h4>Phone: {phone}</h4>
-    </div>
+    <Card>
+      <CardHeader></CardHeader>
+      <CardImage>
+        <Image
+          isRatio='4:3'
+          src={picture.large}
+          alt={`${name.first} headshot`}
+        />
+      </CardImage>
+      <CardContent>
+        <Media>
+          <MediaLeft>
+            <Image isSize='48x48' src={picture.thumbnail} />
+          </MediaLeft>
+          <MediaContent>
+            <Title isSize={4}>
+              {name.first} {name.last}
+            </Title>
+            <Subtitle isSize={6}>
+              @{name.first}
+              {name.last}
+            </Subtitle>
+          </MediaContent>
+          <h3>
+            {location.city} {location.country}
+          </h3>
+        </Media>
+        <Content>
+          {email}
+          <br />
+          {phone}
+        </Content>
+      </CardContent>
+    </Card>
   );
 };
 
